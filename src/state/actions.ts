e507361b8f4fcd56310fcc9dcf76b092b87bb376
KabbalahTreeofLife <castillomarcodaniel@gmail.com>
2026-07-22 15:55:35 +0800

    Add Actions, Reducer, storeContext

---
+++ src/state/actions.ts
@@ -0,0 +1,45 @@
+import { Product } from "./types";
+
+export enum ActionType {
+  ADD_TO_CART = "ADD_TO_CART",
+  REMOVE_FROM_CART = "REMOVE_FROM_CART",
+  UPDATE_QUANTITY = "UPDATE_QUANTITY",
+  CLEAR_CART = "CLEAR_CART",
+  SET_SEARCH_QUERY = "SET_SEARCH_QUERY",
+  SET_CATEGORY = "SET_CATEGORY",
+  SET_SORT = "SET_SORT",
+  TOGGLE_CART = "TOGGLE_CART",
+}
+
+export type Action =
+  | {
+      type: ActionType.ADD_TO_CART;
+      payload: Product;
+    }
+  | {
+      type: ActionType.REMOVE_FROM_CART;
+      payload: string;
+    }
+  | {
+      type: ActionType.UPDATE_QUANTITY;
+      payload: { id: string; quantity: number };
+    }
+  | {
+      type: ActionType.CLEAR_CART;
+    }
+  | {
+      type: ActionType.SET_SEARCH_QUERY;
+      payload: string;
+    }
+  | {
+      type: ActionType.SET_CATEGORY;
+      payload: string;
+    }
+  | {
+      type: ActionType.SET_SORT;
+      payload: string;
+    }
+  | {
+      type: ActionType.TOGGLE_CART;
+      payload?: boolean;
+    };
